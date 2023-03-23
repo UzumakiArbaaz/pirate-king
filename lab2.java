ee3a89b56f7b63d721d6268a3af069af89b253ed
EC2 Default User <ec2-user@ip-172-31-64-103.ec2.internal>
2023-03-23 11:08:29 +0000

    new commit done.

--- lab2.java
+++ lab2.java
@@ -1,5 +1,6 @@
 class lab2 {
     public static void main(String[] args) {
         System.out.println("Hello, World! once again"); 
+         System.out.println("Hello, World! new commit"); 
     }
 }
